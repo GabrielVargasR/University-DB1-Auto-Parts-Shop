1dad78150dfbfe1126af05751f0c3b036a1b5888
Gabriel Vargas <gvargasr@estudiantec.cr>
2020-03-23 02:21:19 -0600

    Create empty main window

--- src/model/IConstants.java
+++ src/model/IConstants.java
@@ -3,4 +3,5 @@ package model;
 public interface IConstants {
 	public static final int WINDOW_WIDTH = 1000;
 	public static final int WINDOW_HEIGHT = 800;
+	public static final String WINDOW_NAME = "Proyecto 1 Bases de Datos 1 - Gabriel Vargas";
 }
--- src/view/UserInterface.java
+++ src/view/UserInterface.java
@@ -1,9 +1,21 @@
 package view;
 
 import javax.swing.*;
+import model.IConstants;
 
-public class UserInterface {
+public class UserInterface implements IConstants{
+	
+	private JFrame frame;
+	
 	public UserInterface() {
-		
+		this.frame = new JFrame(WINDOW_NAME);
+		this.frame.setSize(WINDOW_WIDTH,WINDOW_HEIGHT);  
+		this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.frame.setLayout(null);  
+		this.frame.setVisible(true); 
+	}
+	
+	public static void main(String[] args) {
+		UserInterface ui = new UserInterface();
 	}
 }
