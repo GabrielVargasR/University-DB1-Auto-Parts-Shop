13fc7f8ae9187f4610dabc3045c95c819e6f268e
Gabriel Vargas <gvargasr@estudiantec.cr>
2020-03-23 02:32:50 -0600

    Added menu

--- src/model/IConstants.java
+++ src/model/IConstants.java
@@ -4,4 +4,8 @@ public interface IConstants {
 	public static final int WINDOW_WIDTH = 1000;
 	public static final int WINDOW_HEIGHT = 800;
 	public static final String WINDOW_NAME = "Proyecto 1 Bases de Datos 1 - Gabriel Vargas";
+	
+	public static final String CLIENTS_MENU_TITLE = "Clientes";
+	public static final String PARTS_MENU_TITLE = "Partes";
+	public static final String ORDERS_MENU_TITLE = "Órdenes";
 }
--- src/view/UserInterface.java
+++ src/view/UserInterface.java
@@ -6,13 +6,26 @@ import model.IConstants;
 public class UserInterface implements IConstants{
 	
 	private JFrame frame;
+	private JMenuBar menu;
+	private JMenu clientsMenu, partsMenu, ordersMenu;
 	
 	public UserInterface() {
 		this.frame = new JFrame(WINDOW_NAME);
 		this.frame.setSize(WINDOW_WIDTH,WINDOW_HEIGHT);  
 		this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		this.menu = new JMenuBar();
+		this.clientsMenu = new JMenu(CLIENTS_MENU_TITLE);
+		this.partsMenu = new JMenu(PARTS_MENU_TITLE);
+		this.ordersMenu = new JMenu(ORDERS_MENU_TITLE);
+		
+		this.menu.add(clientsMenu);
+		this.menu.add(partsMenu);
+		this.menu.add(ordersMenu);
+		this.frame.setJMenuBar(this.menu);
+		
 		this.frame.setLayout(null);  
-		this.frame.setVisible(true); 
+		this.frame.setVisible(true);
 	}
 	
 	public static void main(String[] args) {
