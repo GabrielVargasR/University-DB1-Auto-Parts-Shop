042b063281cd90cb5ebee238cec5facf9f50c0b5
Gabriel Vargas <gvargasr@estudiantec.cr>
2020-03-24 00:15:54 -0600

    Added empty ActionListeners to menu

--- src/view/UserInterface.java
+++ src/view/UserInterface.java
@@ -39,6 +39,9 @@ public class UserInterface extends JFrame implements IConstants{
 		this.listClientsItem = new JMenuItem(LIST_CLIENTS_ITEM_TEXT);
 		
 		this.newClientItem.addActionListener(this.newClientMenuListener());
+		this.modClientItem.addActionListener(this.modClientMenuListener());
+		this.suspendClientItem.addActionListener(this.suspendClientMenuListener());
+		this.listClientsItem.addActionListener(this.listClientsMenuListener());
 		
 		this.clientsMenu.add(this.newClientItem);
 		this.clientsMenu.add(this.modClientItem);
@@ -53,6 +56,13 @@ public class UserInterface extends JFrame implements IConstants{
 		this.updatePriceItem = new JMenuItem(UPDATE_PRICE_ITEM_TEXT);
 		this.listCarPartsItem = new JMenuItem(LIST_CAR_PARTS_ITEM_TEXT);
 		
+		this.newPartItem.addActionListener(this.newPartMenuListener());
+		this.deletePartItem.addActionListener(this.deletePartMenuListener());
+		this.addProviderItem.addActionListener(this.addProviderMenuListener());
+		this.addCarTypeItem.addActionListener(this.addCarTypeMenuListener());
+		this.updatePriceItem.addActionListener(this.updatePriceMenuListener());
+		this.listCarPartsItem.addActionListener(this.listCarPartsMenuListener());
+		
 		this.partsMenu.add(this.newPartItem);
 		this.partsMenu.add(this.deletePartItem);
 		this.partsMenu.add(this.addProviderItem);
@@ -65,19 +75,138 @@ public class UserInterface extends JFrame implements IConstants{
 		this.newOrderItem = new JMenuItem(NEW_ORDER_ITEM_TEXT);
 		this.providerDataItem = new JMenuItem(PROVIDER_DATA_ITEM_TEXT);
 		
+		this.locateProviderItem.addActionListener(this.locateProviderMenuListener());
+		this.newOrderItem.addActionListener(this.newOrderMenuListener());
+		this.providerDataItem.addActionListener(this.providerDataMenuListener());
+		
 		this.ordersMenu.add(this.locateProviderItem);
 		this.ordersMenu.add(this.newOrderItem);
 		this.ordersMenu.add(this.providerDataItem);
+		
+		
+		
+		
 		
 		this.menu.add(clientsMenu);
 		this.menu.add(partsMenu);
 		this.menu.add(ordersMenu);
 	}
 	
+	// Client Menu ActionListeners
 	public ActionListener newClientMenuListener() {
 		ActionListener action = new ActionListener() {
 			public void actionPerformed(ActionEvent e){  
-				System.out.println("lskauf");
+				System.out.println("a");
+			}
+		};
+		return action;
+	}
+	
+	public ActionListener modClientMenuListener() {
+		ActionListener action = new ActionListener() {
+			public void actionPerformed(ActionEvent e){  
+				System.out.println("b");
+			}
+		};
+		return action;
+	}
+	
+	public ActionListener suspendClientMenuListener() {
+		ActionListener action = new ActionListener() {
+			public void actionPerformed(ActionEvent e){  
+				System.out.println("c");
+			}
+		};
+		return action;
+	}
+	
+	public ActionListener listClientsMenuListener() {
+		ActionListener action = new ActionListener() {
+			public void actionPerformed(ActionEvent e){  
+				System.out.println("d");
+			}
+		};
+		return action;
+	}
+	
+	// Parts Menu ActionListeners
+	public ActionListener newPartMenuListener() {
+		ActionListener action = new ActionListener() {
+			public void actionPerformed(ActionEvent e){  
+				System.out.println("e");
+			}
+		};
+		return action;
+	}
+	
+	public ActionListener deletePartMenuListener() {
+		ActionListener action = new ActionListener() {
+			public void actionPerformed(ActionEvent e){  
+				System.out.println("f");
+			}
+		};
+		return action;
+	}
+	
+	public ActionListener addProviderMenuListener() {
+		ActionListener action = new ActionListener() {
+			public void actionPerformed(ActionEvent e){  
+				System.out.println("g");
+			}
+		};
+		return action;
+	}
+	
+	public ActionListener addCarTypeMenuListener() {
+		ActionListener action = new ActionListener() {
+			public void actionPerformed(ActionEvent e){  
+				System.out.println("h");
+			}
+		};
+		return action;
+	}
+	
+	public ActionListener updatePriceMenuListener() {
+		ActionListener action = new ActionListener() {
+			public void actionPerformed(ActionEvent e){  
+				System.out.println("i");
+			}
+		};
+		return action;
+	}
+	
+	public ActionListener listCarPartsMenuListener() {
+		ActionListener action = new ActionListener() {
+			public void actionPerformed(ActionEvent e){  
+				System.out.println("j");
+			}
+		};
+		return action;
+	}
+	
+	// Orders Menu ActionListeners
+	public ActionListener locateProviderMenuListener() {
+		ActionListener action = new ActionListener() {
+			public void actionPerformed(ActionEvent e){  
+				System.out.println("k");
+			}
+		};
+		return action;
+	}
+	
+	public ActionListener newOrderMenuListener() {
+		ActionListener action = new ActionListener() {
+			public void actionPerformed(ActionEvent e){  
+				System.out.println("l");
+			}
+		};
+		return action;
+	}
+	
+	public ActionListener providerDataMenuListener() {
+		ActionListener action = new ActionListener() {
+			public void actionPerformed(ActionEvent e){  
+				System.out.println("m");
 			}
 		};
 		return action;
