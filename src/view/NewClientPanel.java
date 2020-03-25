d882ecceddaf8e6d036bedbf32e4c289b218920c
Gabriel Vargas <gvargasr@estudiantec.cr>
2020-03-24 19:30:44 -0600

    Panel change test

--- src/view/NewClientPanel.java
+++ src/view/NewClientPanel.java
@@ -1,9 +1,12 @@
 package view;
 
+import java.awt.Color;
 import javax.swing.*;
 
 public class NewClientPanel extends JPanel{
 	public NewClientPanel() {
-		
+		super();
+		this.setBackground(Color.BLUE);
+		this.setSize(500,500);
 	}
 }
