cae2e5eaee121522c17114c9706651b54bbc39dd
Gabriel Vargas <gvargasr@estudiantec.cr>
2020-04-02 16:50:11 -0600

    Worked on class methods

--- src/model/DataBaseCommunicator.java
+++ src/model/DataBaseCommunicator.java
@@ -6,23 +6,53 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class DataBaseCommunicator {
+public class DataBaseCommunicator implements IConstants{
+	
+	public DataBaseCommunicator() { }
+	
+	public ResultSet queryDB(String pQuery) {
+		try (Connection connection = DriverManager.getConnection(DB_CONNECTION_URL); Statement statement = connection.createStatement();) {
+	          ResultSet resultSet = statement.executeQuery(pQuery);
+	          return resultSet;
+	      }
+	      // Handle any errors that may have occurred.
+	      catch (SQLException e) {
+	          e.printStackTrace();
+	          return null;
+	      }
+	}
+	
 	public static void main(String[] args) {
 
-		String connectionUrl = "jdbc:sqlserver://localhost:1433;databaseName=Progra1;user=SA;password=<Progra1Bases1>";
-
-      try (Connection con = DriverManager.getConnection(connectionUrl); Statement stmt = con.createStatement();) {
+      try (Connection con = DriverManager.getConnection(DB_CONNECTION_URL); Statement stmt = con.createStatement();) {
           String SQL = "SELECT * FROM test_table";
           ResultSet rs = stmt.executeQuery(SQL);
+          
+          rs.updateString("str", "prueba");
+    	  rs.updateRow();
 
           // Iterate through the data in the result set and display it.
           while (rs.next()) {
               System.out.println(rs.getInt("num") + " " + rs.getString("str"));
           }
+          
+          rs.absolute(1);
+          rs.updateString("str", "prueba");
+    	  rs.updateRow();
       }
       // Handle any errors that may have occurred.
       catch (SQLException e) {
           e.printStackTrace();
       }
+//		DataBaseCommunicator dbc = new DataBaseCommunicator();
+//		ResultSet rs = dbc.queryDB("SELECT * FROM test_table");
+//		try {
+//			while (rs.next()) {
+//				System.out.println(rs.getInt("num") + " " + rs.getString("str"));
+//			}
+//		} catch (SQLException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
   }
 }
