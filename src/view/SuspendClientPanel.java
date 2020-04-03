b273b1b2968b3da7706593627f145dd99db3b3dd
Gabriel Vargas <gvargasr@estudiantec.cr>
2020-04-03 12:47:28 -0600

    Finished draft for mod and suspend client panels

--- src/view/SuspendClientPanel.java
+++ src/view/SuspendClientPanel.java
@@ -1,11 +1,28 @@
 package view;
 
+import java.awt.FlowLayout;
+
 import javax.swing.*;
 
 public class SuspendClientPanel extends JPanel implements IUIConstants{
+	
+	private JLabel cedulaLabel;
+	private JTextField cedulaEntry;
+	private JButton suspendButton;
+	
 	public SuspendClientPanel() {
 		super();
 		super.setBounds(PANEL_X, PANEL_Y, PANEL_WIDTH, PANEL_HEIGHT);
 		super.setOpaque(true);
+		
+		super.setLayout(new FlowLayout(FlowLayout.CENTER, 300, 40));
+		
+		this.cedulaLabel = new JLabel("Cédula:");
+		this.cedulaEntry = new JTextField(20);
+		this.suspendButton = new JButton("Suspender");
+		
+		this.add(this.cedulaLabel);
+		this.add(cedulaEntry);
+		this.add(this.suspendButton);
 	}
 }
