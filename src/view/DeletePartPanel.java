dc4e941cd0cf046644a4b51f6b76b5968ada5c07
Gabriel Vargas <gvargasr@estudiantec.cr>
2020-04-04 12:53:18 -0600

    Finished draft for NewPart and DeletePart panel

--- src/view/DeletePartPanel.java
+++ src/view/DeletePartPanel.java
@@ -1,13 +1,30 @@
 package view;
 
+import java.awt.FlowLayout;
+
 import javax.swing.*;
 
 public class DeletePartPanel extends JPanel implements IUIConstants{
+	
+	private JLabel partNameLabel;
+	private JTextField partNameEntry;
+	private JButton delete;
 
 	public DeletePartPanel() {
 		super();
 		super.setBounds(PANEL_X, PANEL_Y, PANEL_WIDTH, PANEL_HEIGHT);
 		super.setOpaque(true);
+		
+		super.setLayout(new FlowLayout(FlowLayout.CENTER, 300, 40));
+		
+		this.partNameLabel = new JLabel("Nombre:");
+		this.add(this.partNameLabel);
+		
+		this.partNameEntry = new JTextField(20);
+		this.add(this.partNameEntry);
+		
+		this.delete = new JButton("Borrar");
+		this.add(this.delete);
 	}
 
 }
