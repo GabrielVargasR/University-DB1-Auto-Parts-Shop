9bb03dc9d83c1441358c4b83b519efc63229cd69
Gabriel Vargas <gvargasr@estudiantec.cr>
2020-05-30 17:51:23 -0600

    Fixed organizaciones mistakes

--- src/sql_scripts/read/ReadOrganizaciones.sql
+++ src/sql_scripts/read/ReadOrganizaciones.sql
@@ -1,9 +1,9 @@
 DELIMITER //
-
+DROP PROCEDURE IF EXISTS ReadOrganizaciones;
 CREATE PROCEDURE ReadOrganizaciones()
 BEGIN
-	SELECT v.id, v.cedula, v.nombre, v.telefono, v.direccion, v.ciudad, v.estado
-    FROM vClientePersona AS v;
+	SELECT v.id, v.cedula, v.nombre, v.telefono, v.direccion, v.ciudad, v.estado, v.tel_contacto
+    FROM vclienteorganizacion AS v;
 END //
 
 DELIMITER ;
